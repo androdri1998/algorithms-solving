f35dff0f8bcd718c4dbc85d8944397d288278acc
André Rodrigues <andre.rodrigues98@outlook.com>
2022-03-09 20:57:17 -0300

    fix Simple Array Sum problem

--- nodeJS/SimpleArraySum/index.js
+++ nodeJS/SimpleArraySum/index.js
@@ -1,47 +1,7 @@
-const checkConstraintsToLengthOfArray = (arrayLength) => {
-  if (arrayLength > 0) {
-    return true;
-  }
-
-  return false;
-}
-
-const checkConstraintsToArrayString = (arrayString) => {
-  const arrayToCheck = arrayString.trim().split(' ');
-  if (arrayToCheck.length <= 1000) {
-    return true;
-  }
-
-  return false;
-}
-
-const checkLengthOfArray = (arrayLength, arrayString) => {
-  const arrayToCheck = arrayString.trim().split(' ');
-  const isEqual = arrayLength === arrayToCheck.length;
-
-  return isEqual
-}
-
-const sumArray = (arrayString) => {
-  const arrayToSum = arrayString.trim().split(' ');
-
-  const sum = arrayToSum.reduce((accumulator, currentValue) => {
-    return parseInt(accumulator) + parseInt(currentValue);
+function solution(ar) {
+  const sum = ar.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
   }, 0);
 
-  return sum
+  return sum;
 }
-
-const sumValues = (arrayLength, arrayString) => {
-  if(checkConstraintsToLengthOfArray(arrayLength) && 
-    checkConstraintsToArrayString(arrayString) && 
-    checkLengthOfArray(arrayLength, arrayString)) {
-    const sum = sumArray(arrayString);
-
-    return sum;
-  } else {
-    throw new Error("Incorrect Parameters!");
-  }
-}
-
-console.log(sumValues(5, "4 3 2 1 2"));
