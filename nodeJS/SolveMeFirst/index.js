0918a331d317f018a3038b8aed98c69282350a6d
André Rodrigues <andre.rodrigues98@outlook.com>
2022-03-09 20:50:53 -0300

    fix Solve Me First problem

--- nodeJS/SolveMeFirst/index.js
+++ nodeJS/SolveMeFirst/index.js
@@ -1,26 +1,4 @@
-const checkConstraintsToFirstVariable = (variable) => {
-  if (variable >= 1) {
-    return true;
-  }
-
-  return false;
+function solution(a, b) {
+  const sum = a + b;
+  return sum;
 }
-
-const checkConstraintsToSecondVariable = (variable) => {
-  if (variable <= 1000) {
-    return true;
-  }
-
-  return false;
-}
-
-const sumValues = (firstNumber, secondNumber) => {
-  if(checkConstraintsToFirstVariable(firstNumber) && checkConstraintsToSecondVariable(secondNumber)) {
-    const sum = firstNumber + secondNumber;
-    return sum;
-  } else {
-    throw new Error("Incorrect Parameters!");
-  }
-}
-
-console.log(sumValues(3, 4));
