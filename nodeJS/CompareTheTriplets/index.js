048f50a438ebd47d9592a77f940761a8d2011956
André Rodrigues <andre.rodrigues98@outlook.com>
2022-03-09 21:09:38 -0300

    fix Compare the Triplets problem

--- nodeJS/CompareTheTriplets/index.js
+++ nodeJS/CompareTheTriplets/index.js
@@ -1,40 +1,18 @@
-const checkConstraintsInput = (arrayString) => {
-  const arrayNumbers = arrayString.trim().split(' ');
-  let isValid = true;
-  arrayNumbers.forEach(number => {
-    if(parseInt(number) < 1 || parseInt(number) > 100) {
-      isValid = false;
-    }
-  });
 
-  return isValid;
-}
-
-const compareTriplets = (a, b) => {
-  const aArrayNumbers = a.trim().split(' ');
-  const bArrayNumbers = b.trim().split(' ');
-
+function solution(a, b) {
   let aScore = 0;
   let bScore = 0;
 
-  aArrayNumbers.forEach((aNumber, index) => {
-    if(parseInt(aNumber) > parseInt(bArrayNumbers[index])) {
-      aScore += 1;
-    } else if(parseInt(aNumber) < parseInt(bArrayNumbers[index])) {
-      bScore += 1;
+  for (let index = 0; index < a.length; index++) {
+    if (a[index] > b[index]) {
+      aScore++;
+      continue;
     }
-  });
+    if (a[index] < b[index]) {
+      bScore++;
+      continue;
+    }
+  }
 
   return [aScore, bScore];
 }
-
-const checkTriplets = (a, b) => {
-  if(checkConstraintsInput(a) && checkConstraintsInput(b)) {
-    const checkedTriplets = compareTriplets(a, b);
-    return checkedTriplets;
-  } else {
-    throw new Error("Incorrect Parameters!");
-  }
-}
-
-console.log(checkTriplets("3 2 11", "3 1 12"));
